8f78ce041031176a794afdac6a20bba0f168b412
AmitKumar <amitkumar050497@gmail.com>
2025-01-05 12:09:56 +0530

    Adding Hello world file

--- src/learning_java/Lab001_HelloWorld.java
+++ src/learning_java/Lab001_HelloWorld.java
@@ -1,4 +1,6 @@
-public class L10x_HelloWorld {
+package learning_java;
+
+public class Lab001_HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World");
     }
